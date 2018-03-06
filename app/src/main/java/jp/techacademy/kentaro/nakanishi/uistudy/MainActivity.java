00b39077f0572685dfb91a9099bfca08a8741229
kentaronakanishi <kentaronakanishi1987@gmail.com>
2018-03-06 20:43:03 +0900

    問題があります。よろしくお願いします。

---
+++ app/src/main/java/jp/techacademy/kentaro/nakanishi/uistudy/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.techacademy.kentaro.nakanishi.uistudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setText("ボタン");
+    }
+}
